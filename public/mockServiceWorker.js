db368461b4063788d9f28f79d2427abda6a7d4d5
Climier-code <ikowhddus@naver.com>
2025-12-04 21:03:23 +0900

    fix: mock server 오류 수정

--- public/mockServiceWorker.js
+++ public/mockServiceWorker.js
@@ -1,3 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+
+/**
+ * Mock Service Worker.
+ * @see https://github.com/mswjs/msw
+ * - Please do NOT modify this file.
+ */
 
 const PACKAGE_VERSION = '2.12.3'
 const INTEGRITY_CHECKSUM = '4db4a41e972cec1b64cc569c66952d82'
@@ -70,6 +78,7 @@ addEventListener('message', async function (event) {
         return client.id !== clientId
       })
 
+      // Unregister itself when there are no more clients
       if (remainingClients.length === 0) {
         self.registration.unregister()
       }
@@ -82,10 +91,13 @@ addEventListener('message', async function (event) {
 addEventListener('fetch', function (event) {
   const requestInterceptedAt = Date.now()
 
+  // Bypass navigation requests.
   if (event.request.mode === 'navigate') {
     return
   }
 
+  // Opening the DevTools triggers the "only-if-cached" request
+  // that cannot be handled by the worker. Bypass such requests.
   if (
     event.request.cache === 'only-if-cached' &&
     event.request.mode !== 'same-origin'
@@ -93,6 +105,9 @@ addEventListener('fetch', function (event) {
     return
   }
 
+  // Bypass all requests when there are no active clients.
+  // Prevents the self-unregistered worked from handling requests
+  // after it's been terminated (still remains active until the next reload).
   if (activeClientIds.size === 0) {
     return
   }
@@ -101,6 +116,11 @@ addEventListener('fetch', function (event) {
   event.respondWith(handleRequest(event, requestId, requestInterceptedAt))
 })
 
+/**
+ * @param {FetchEvent} event
+ * @param {string} requestId
+ * @param {number} requestInterceptedAt
+ */
 async function handleRequest(event, requestId, requestInterceptedAt) {
   const client = await resolveMainClient(event)
   const requestCloneForEvents = event.request.clone()
@@ -111,9 +131,13 @@ async function handleRequest(event, requestId, requestInterceptedAt) {
     requestInterceptedAt,
   )
 
+  // Send back the response clone for the "response:*" life-cycle events.
+  // Ensure MSW is active and ready to handle the message, otherwise
+  // this message will pend indefinitely.
   if (client && activeClientIds.has(client.id)) {
     const serializedRequest = await serializeRequest(requestCloneForEvents)
 
+    // Clone the response so both the client and the library could consume it.
     const responseClone = response.clone()
 
     sendToClient(
@@ -142,6 +166,14 @@ async function handleRequest(event, requestId, requestInterceptedAt) {
   return response
 }
 
+/**
+ * Resolve the main client for the given event.
+ * Client that issues a request doesn't necessarily equal the client
+ * that registered the worker. It's with the latter the worker should
+ * communicate with during the response resolving phase.
+ * @param {FetchEvent} event
+ * @returns {Promise<Client | undefined>}
+ */
 async function resolveMainClient(event) {
   const client = await self.clients.get(event.clientId)
 
@@ -159,19 +191,36 @@ async function resolveMainClient(event) {
 
   return allClients
     .filter((client) => {
+      // Get only those clients that are currently visible.
       return client.visibilityState === 'visible'
     })
     .find((client) => {
+      // Find the client ID that's recorded in the
+      // set of clients that have registered the worker.
       return activeClientIds.has(client.id)
     })
 }
 
+/**
+ * @param {FetchEvent} event
+ * @param {Client | undefined} client
+ * @param {string} requestId
+ * @param {number} requestInterceptedAt
+ * @returns {Promise<Response>}
+ */
 async function getResponse(event, client, requestId, requestInterceptedAt) {
+  // Clone the request because it might've been already used
+  // (i.e. its body has been read and sent to the client).
   const requestClone = event.request.clone()
 
   function passthrough() {
+    // Cast the request headers to a new Headers instance
+    // so the headers can be manipulated with.
     const headers = new Headers(requestClone.headers)
 
+    // Remove the "accept" header value that marked this request as passthrough.
+    // This prevents request alteration and also keeps it compliant with the
+    // user-defined CORS policies.
     const acceptHeader = headers.get('accept')
     if (acceptHeader) {
       const values = acceptHeader.split(',').map((value) => value.trim())
@@ -189,14 +238,20 @@ async function getResponse(event, client, requestId, requestInterceptedAt) {
     return fetch(requestClone, { headers })
   }
 
+  // Bypass mocking when the client is not active.
   if (!client) {
     return passthrough()
   }
 
+  // Bypass initial page load requests (i.e. static assets).
+  // The absence of the immediate/parent client in the map of the active clients
+  // means that MSW hasn't dispatched the "MOCK_ACTIVATE" event yet
+  // and is not ready to handle requests.
   if (!activeClientIds.has(client.id)) {
     return passthrough()
   }
 
+  // Notify the client that a request has been intercepted.
   const serializedRequest = await serializeRequest(event.request)
   const clientMessage = await sendToClient(
     client,
@@ -224,6 +279,12 @@ async function getResponse(event, client, requestId, requestInterceptedAt) {
   return passthrough()
 }
 
+/**
+ * @param {Client} client
+ * @param {any} message
+ * @param {Array<Transferable>} transferrables
+ * @returns {Promise<any>}
+ */
 function sendToClient(client, message, transferrables = []) {
   return new Promise((resolve, reject) => {
     const channel = new MessageChannel()
@@ -243,7 +304,15 @@ function sendToClient(client, message, transferrables = []) {
   })
 }
 
+/**
+ * @param {Response} response
+ * @returns {Response}
+ */
 function respondWithMock(response) {
+  // Setting response status code to 0 is a no-op.
+  // However, when responding with a "Response.error()", the produced Response
+  // instance will have status code set to 0. Since it's not possible to create
+  // a Response instance with status code 0, handle that use-case separately.
   if (response.status === 0) {
     return Response.error()
   }
@@ -258,6 +327,9 @@ function respondWithMock(response) {
   return mockedResponse
 }
 
+/**
+ * @param {Request} request
+ */
 async function serializeRequest(request) {
   return {
     url: request.url,
